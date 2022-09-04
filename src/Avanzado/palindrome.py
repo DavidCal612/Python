667770851395f86f572a341ce99c0ed0b146a219
David <davscalderon@misena.edu.co>
2022-09-04 17:46:32 -0500

    commit

--- src/Avanzado/palindrome.py
+++
@@ -1,9 +0,0 @@
-def is_palindrome(string: str) -> bool:
-    string = string.replace(" ", " ").lower()
-    return string == string[::-1]
-
-def run():
-    print(is_palindrome("ana")) 
-    
-    if __name__ == '__main__':
-        run()
