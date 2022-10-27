a4f84894e2031ea6d5e0b0f70b523931dfbbb7f0
David <davscalderon@misena.edu.co>
2022-10-26 21:08:10 -0500

    django

--- src/Django/premiosplatziapp/premiosplatziapp/asgi.py
+++ src/Django/premiosplatziapp/premiosplatziapp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Djangoapp project.
+ASGI config for premiosplatziapp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Djangoapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'premiosplatziapp.settings')
 
 application = get_asgi_application()
--- src/Django/premiosplatziapp/premiosplatziapp/wsgi.py
+++ src/Django/premiosplatziapp/premiosplatziapp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Djangoapp project.
+WSGI config for premiosplatziapp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Djangoapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'premiosplatziapp.settings')
 
 application = get_wsgi_application()
