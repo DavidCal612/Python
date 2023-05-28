b038ce24e6bd352c919f6162ff078a6f64e6a3bf
David <davscalderon@misena.edu.co>
2023-05-27 20:41:46 -0500

    27/05/23 Vistas

--- src/Django/premiosplatzi/premiosplatziapp/polls/urls.py
+++ src/Django/premiosplatzi/premiosplatziapp/polls/urls.py
@@ -2,7 +2,17 @@ from django.urls import path
 
 from . import views
 
+# CREACION DE VISTAS EN PYTHON 
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    #ex: /polls/5/
+    path("<int:question_id>/", views.detail, name="index"),
+    #ex: /polls/5/
+    path("<int:question_id>/results/", views.results, name="index"),
+    #ex: /polls/5/
+    path("<int:question_id>/vote/", views.vote, name="index"),
 ]
+
+# int:question_id>/ para poder pasar parametros mediante la URL que estamps utilizando para una pagina web 
 
--- src/Django/premiosplatzi/premiosplatziapp/polls/views.py
+++ src/Django/premiosplatzi/premiosplatziapp/polls/views.py
@@ -3,6 +3,20 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse(" Estas en la pagina principal  ")
 
 # Create your views here.
+
+#
+
+def detail(request, question_id):
+    return  HttpResponse(f"Estas viendo la pregunta numero {question_id} ")
+
+
+def results(request, question_id):
+    return  HttpResponse(f"Estas viendo los resultados de la pregunta {question_id} ")
+
+
+def vote(request, question_id):
+    return  HttpResponse(f"Estas votando la pregunta numero {question_id} ")
+    
