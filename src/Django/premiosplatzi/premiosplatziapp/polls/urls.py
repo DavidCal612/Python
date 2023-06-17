47cd790179ca99dc3c629c77d07d88d1ee2c0f84
David <davscalderon@misena.edu.co>
2023-06-17 18:52:49 -0500

    django

--- src/Django/premiosplatzi/premiosplatziapp/polls/urls.py
+++ src/Django/premiosplatzi/premiosplatziapp/polls/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 
 from . import views
 
-# CREACION DE VISTAS EN PYTHON 
-
+# CREACION DE VISTAS EN PYTHON  # /polls/{{question.id}} 
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
     #ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="index"),
+    path("<int:question_id>/detail/paginadetails", views.detail, name="detail"),
     #ex: /polls/5/
-    path("<int:question_id>/results/", views.results, name="index"),
+    path("<int:question_id>/results/", views.results, name="results"),
     #ex: /polls/5/
-    path("<int:question_id>/vote/", views.vote, name="index"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
 
 # int:question_id>/ para poder pasar parametros mediante la URL que estamps utilizando para una pagina web 
--- src/Django/premiosplatzi/premiosplatziapp/polls/views.py
+++ src/Django/premiosplatzi/premiosplatziapp/polls/views.py
@@ -1,18 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-
-def index(request):
-    return HttpResponse(" Estas en la pagina principal  ")
+from .models import Question
 
 # Create your views here.
 
 #
 
+def index(request):
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
+
+
 def detail(request, question_id):
-    return  HttpResponse(f"Estas viendo la pregunta numero {question_id} ")
-
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
+    
+    
 def results(request, question_id):
     return  HttpResponse(f"Estas viendo los resultados de la pregunta {question_id} ")
 
